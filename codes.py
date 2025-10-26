9f7bcb3b108be5f636fea78a2cb4d6be08994da3
qwe2qwe <qwe2qwe@hotmail.com>
2025-10-26 14:38:01 +0300

    Data Types and Type Conversion Exercises varibels

---
+++ codes.py
@@ -0,0 +1,81 @@
+#day 2: Data Types and Type Conversion Exercises
+
+#Exercise 1
+name = str('Darth Vader')
+print(name)
+
+age = int(19)
+print(age)
+
+#Exercise 2
+
+i_cannot_do_it=False
+print(i_cannot_do_it)
+
+the_answer= float(42.01)
+
+print(the_answer)
+
+#Exercise 3
+height = str('1.09')
+
+print(height)
+
+height = float(height)
+
+height = height + 0.66
+
+print(height)
+
+
+
+# these are test with chatgpt
+
+# test 1
+F = float(2.88)
+print(F)
+print(type(F))
+N = int(5)
+
+print(N)
+print(type(N))
+
+#test2 
+name = str("hS cooding")
+
+print('Hello, my name is ' + name)
+
+# test 3 
+
+is_sunny = True
+
+if is_sunny:
+    print("It's a sunny day!")
+else:
+    print("It's cloudy.")
+
+# test 4
+
+a = int(20 )       # int
+print(type(a))
+
+b = float(4.5)       # float
+print(type(b))
+
+c = str("Python")  # str
+print(type(c))
+
+d = bool(False)     # bool
+print(type(d))
+
+# test 5
+
+num_str = "50"
+
+num_str = int(num_str) * 2
+
+print(num_str)
+print(type(num_str))
+
+
+
