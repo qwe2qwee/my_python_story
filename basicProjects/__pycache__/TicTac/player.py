ff9f0132c121ea230318df9a5ff7ed97ec79bb96
qwe2qwe <qwe2qwe@hotmail.com>
2025-12-03 20:01:11 +0300

    Add GeniusComputerPlayer class and update game logic to use it

--- basicProjects/__pycache__/TicTac/player.py
+++ basicProjects/__pycache__/TicTac/player.py
@@ -41,3 +41,71 @@ class HumanPlayer(Player):
                 print("Invalid square. Try again.")
 
         return val
+class GeniusComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            square = random.choice(game.available_moves())
+        else:
+            square = self.minimax(game, self.letter)['position']
+        return square
+
+    def minimax(self, state, player):
+        max_player = self.letter
+        other_player = 'O' if player == 'X' else 'X'
+
+        # Base cases
+        if state.current_winner == other_player:
+            return {
+                'position': None,
+                'score': 1 * (state.num_empty_squares() + 1)
+                if other_player == max_player
+                else -1 * (state.num_empty_squares() + 1)
+            }
+
+        elif not state.empty_squares():
+            return {'position': None, 'score': 0}
+
+        # Initialize best
+        if player == max_player:
+            best = {'position': None, 'score': -math.inf}
+        else:
+            best = {'position': None, 'score': math.inf}
+
+        # Explore moves
+        for possible_move in state.available_moves():
+            state.make_move(possible_move, player)
+            sim_score = self.minimax(state, other_player)
+
+            # Undo move
+            state.board[possible_move] = ' '
+            state.current_winner = None
+            sim_score['position'] = possible_move
+
+            # Update best
+            if player == max_player:
+                if sim_score['score'] > best['score']:
+                    best = sim_score
+            else:
+                if sim_score['score'] < best['score']:
+                    best = sim_score
+
+        return best
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
